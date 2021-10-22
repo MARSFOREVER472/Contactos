e3b99e513378ace6ac839a00743e76753ce01033
Marcelo Lam <marcelo.lam@alumnos.ucn.cl>
2021-10-21 21:49:49 -0300

    Initial commit

---
+++ app/src/main/java/cl/ucn/disc/dsm/mlam/contactos/MainActivity.java
@@ -0,0 +1,14 @@
+package cl.ucn.disc.dsm.mlam.contactos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
